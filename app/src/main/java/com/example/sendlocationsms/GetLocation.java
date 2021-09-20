529a4ac4e8c5df9aaa92f1aa40b2309fee758dfd
ZombieCloud <konstantin.gladky@gmail.com>
2021-09-20 14:58:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sendlocationsms/GetLocation.java
@@ -0,0 +1,281 @@
+package com.example.sendlocationsms;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
+import android.os.Looper;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class GetLocation{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    //Constant used in the location settings dialog.
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+
+    private static final String SMS_SENT_ACTION = "SMS_SENT_ACTION";
+    private static final String SMS_DELIVERED_ACTION = "SMS_DELIVERED_ACTION";
+
+
+    //Provides access to the Location Settings API.
+    private SettingsClient mSettingsClient;
+
+     //Stores the types of location services the client is interested in using. Used for checking
+     //settings to determine if the device has optimal location settings.
+    private LocationSettingsRequest mLocationSettingsRequest;
+
+    //Provides access to the Fused Location Provider API.
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    //Stores parameters for requests to the FusedLocationProviderApi.
+    private LocationRequest mLocationRequest;
+
+    /**
+     * Represents a geographical location.
+     */
+    private Location mCurrentLocation;
+
+
+    //Callback for Location events.
+    private LocationCallback mLocationCallback;
+
+    /**
+     * Tracks the status of the location updates request. Value changes when the user presses the
+     * Start Updates and Stop Updates buttons.
+     */
+    private Boolean mRequestingLocationUpdates;
+
+
+    public String latitude = "";
+    public String longitude = "";
+    public String mLastUpdateTime = "";
+
+
+
+//    public Activity mainActivity;
+    public String telNumber;
+    public Context context;
+    public GetLocation(String telNumber1, Context context1) {
+
+        telNumber = telNumber1;
+        context = context1;
+//        Toast.makeText(context, "Tel = " + telNumber, Toast.LENGTH_LONG).show();   //getApplicationContext
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        mSettingsClient = LocationServices.getSettingsClient(context);
+
+//        mLastUpdateTime = "";
+        mRequestingLocationUpdates = false;
+
+        // Kick off the process of building the LocationCallback, LocationRequest, and
+        // LocationSettingsRequest objects.
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingsRequest();
+
+        // START
+        startUpdatesButtonHandler();
+    }
+
+
+
+
+    public void startUpdatesButtonHandler() {
+        if (!mRequestingLocationUpdates) {
+            mRequestingLocationUpdates = true;
+            startLocationUpdates();
+        }
+    }
+
+
+    private void startLocationUpdates() {
+        // Begin by checking if the device has the necessary location settings.
+        mSettingsClient = LocationServices.getSettingsClient(context);
+
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i(TAG, "All location settings are satisfied.");
+
+                        //noinspection MissingPermission
+                        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                                mLocationCallback, Looper.myLooper());
+
+                        updateUI();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        SmsManager sms = SmsManager.getDefault();
+
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+//                                Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade location settings ");
+
+                                sms.sendTextMessage(telNumber, null, "Check app permissions, or Turn on location, or No location sources are available", PendingIntent.getBroadcast(
+                                        context, 0, new Intent(SMS_SENT_ACTION), 0), PendingIntent.getBroadcast(context, 0, new Intent(SMS_DELIVERED_ACTION), 0));
+
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+//                                String errorMessage = "Location settings are inadequate, and cannot be fixed here. Fix in Settings.";
+//                                Log.e(TAG, errorMessage);
+//                                Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show();
+
+                                sms.sendTextMessage(telNumber, null, "Check app permissions, or Turn on location, or No location sources are available", PendingIntent.getBroadcast(
+                                        context, 0, new Intent(SMS_SENT_ACTION), 0), PendingIntent.getBroadcast(context, 0, new Intent(SMS_DELIVERED_ACTION), 0));
+
+                                mRequestingLocationUpdates = false;
+                        }
+
+//                        updateUI();
+                    }
+                }
+                );
+    }
+
+
+    private void updateUI() {
+        updateLocationUI();
+    }
+
+
+    private void updateLocationUI() {
+
+        if (mCurrentLocation != null) {
+
+            latitude = String.format("%f", mCurrentLocation.getLatitude());
+            longitude = String.format("%f", mCurrentLocation.getLongitude());
+            latitude = latitude.replace(",",".");
+            longitude = longitude.replace(",",".");
+
+            // Только один раз получаем данные. Без этого - постоянное обновление
+            stopLocationUpdates();
+
+            Toast.makeText(context, "Latitude = " + latitude + "     " + "Longitude = " + longitude + "    " + "Tel = " + telNumber, Toast.LENGTH_LONG).show();
+
+            //  Отсылаем смс
+            String msgLocation = "http://maps.google.com/?q=" + latitude + "," + longitude;
+
+            if (telNumber != null) {
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(telNumber, null, msgLocation, PendingIntent.getBroadcast(
+                        context, 0, new Intent(SMS_SENT_ACTION), 0), PendingIntent.getBroadcast(context, 0, new Intent(SMS_DELIVERED_ACTION), 0));
+            }
+
+        } else {
+//            Log.i(TAG, "No location");   // Сюда сначала попадает, а потом определяет
+//            Toast.makeText(mainActivity, "No location", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+    private void createLocationCallback() {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                mCurrentLocation = locationResult.getLastLocation();
+                mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+                updateLocationUI();
+            }
+        };
+    }
+
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+
+
+    private void stopLocationUpdates() {
+        if (!mRequestingLocationUpdates) {
+            Log.d(TAG, "stopLocationUpdates: updates never requested, no-op.");
+
+            return;
+        }
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        mRequestingLocationUpdates = false;
+
+        // It is a good practice to remove location requests when the activity is in a paused or
+        // stopped state. Doing so helps battery performance and is especially
+        // recommended in applications that request frequent location updates.
+//        mFusedLocationClient.removeLocationUpdates(mLocationCallback)
+//                .addOnCompleteListener(getParent(), new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        mRequestingLocationUpdates = false;
+//                    }
+//                });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sendlocationsms/SmsReceiver.java
@@ -0,0 +1,79 @@
+package com.example.sendlocationsms;
+
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    private static final String TAG = SmsReceiver.class.getSimpleName();
+    public static final String pdu_type = "pdus";
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // Get the SMS message.
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs;
+        String strMessage = "";
+        String format = bundle.getString("format");
+
+        // Retrieve the SMS message received.
+        try {
+            Object[] pdus = (Object[]) bundle.get(pdu_type);
+
+            if (pdus != null) {
+                // Check the Android version.
+                boolean isVersionM = (Build.VERSION.SDK_INT >=
+                        Build.VERSION_CODES.M);
+
+                // Fill the msgs array.
+                msgs = new SmsMessage[pdus.length];
+                for (int i = 0; i < msgs.length; i++) {
+                    // Check Android version and use appropriate createFromPdu.
+                    if (isVersionM) {
+                        // If Android version M or newer:
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                    } else {
+                        // If Android version L or older:
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    }
+
+                    // Build the message to show.
+                    strMessage += "SMS from " + msgs[i].getOriginatingAddress();
+                    strMessage += " :" + msgs[i].getMessageBody() + "\n";
+
+                    // Log and display the SMS message.
+//                    Log.d(TAG, "PRIVETTT_8: " + strMessage);
+//                    Toast.makeText(context, strMessage, Toast.LENGTH_LONG).show();
+
+                    // Стартуем MainActivity, при старте определяем координаты и ответная смс
+                    String keyString = "WhereAreYouuu";
+                    if (msgs[i].getMessageBody().toLowerCase().contains(keyString.toLowerCase())) {
+                        GetLocation gl = new GetLocation(msgs[i].getOriginatingAddress(), context);
+
+
+//                        Intent mIntent = new Intent(context, MainActivity.class);
+//                        mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME | Intent.FLAG_ACTIVITY_CLEAR_TASK);   // FLAG_ACTIVITY_CLEAR_TASK - для того, чтоб MainActivity рестартовалась при каждой смс
+//                        mIntent.putExtra("STR_TEL_NUMBER", msgs[i].getOriginatingAddress());
+//                        mIntent.putExtra("STR_MESSAGE", msgs[i].getMessageBody());
+//                        context.startActivity(mIntent);
+                    }
+                }
+        }
+
+        } catch(Exception e) {
+                            Log.d("Exception caught",e.getMessage());
+        }
+
+
+//        throw new UnsupportedOperationException("Not yet implemented");   // privet
+    }
+}
