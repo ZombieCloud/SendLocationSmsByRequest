ce717074436ca36db431df3ff3b0ddf4f87d986c
ZombieCloud <konstantin.gladky@gmail.com>
2021-11-19 00:00:48 +0200

    saving the state of check boxes added

--- app/src/main/java/com/rnc/sendlocationsmsbyrequest/MainActivity.java
+++ app/src/main/java/com/rnc/sendlocationsmsbyrequest/MainActivity.java
@@ -15,6 +15,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,14 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnStartStop;
     TextView tvSecretWord;
+    CheckBox checkBox_WakeUp;
+    CheckBox checkBox_ScreenOn;
+    CheckBox checkBox_LaunchMaps;
+
     private static final String TAG = MainActivity.class.getSimpleName();
     private final SmsReceiver smsReceiver = new SmsReceiver();
     private Boolean isServiceRunning;
     private final int MY_KEY_FOR_RETURNED_VALUE = 0;
     private SharedPreferences settings;             // хранилище переменных
     private SharedPreferences.Editor editor;
+
     private String secretWord;
     private String sendResponse;
+
+    private String cbWakeUp;
+    private String cbScreenOn;
+    private String cbLaunchMaps;
 
 
     @Override
@@ -39,11 +49,60 @@ public class MainActivity extends AppCompatActivity {
 
         btnStartStop = findViewById(R.id.btnStartStop);
         tvSecretWord = findViewById(R.id.tvSecretWord);
+        checkBox_WakeUp = findViewById(R.id.checkBox_WakeUp);
+        checkBox_ScreenOn = findViewById(R.id.checkBox_ScreenOn);
+        checkBox_LaunchMaps = findViewById(R.id.checkBox_LaunchMaps);
 //        btnStartStop.setText("Start");
 //        isServiceRunning = false;
 
         settings = this.getSharedPreferences("SLSbR_STORAGE", Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS);  // инициируем хранилище переменных   MODE_MULTI_PROCESS - чтоб было доступно из broadcast receiver
         editor = settings.edit();
+
+
+        //  Достаем checkBox_WakeUp из хранилища
+        cbWakeUp = settings.getString( "state_WakeUp", null);    // достаем переменную из хранилища
+        if (cbWakeUp == null) {
+            cbWakeUp = "0";
+            editor.putString( "state_WakeUp", cbWakeUp);   // положить переменную в хранилище
+            editor.commit();
+        }
+        if ((cbWakeUp.equals("1"))) {
+            checkBox_WakeUp.setChecked(true);
+        } else {
+            checkBox_WakeUp.setChecked(false);
+        }
+
+
+
+        //  Достаем checkBox_ScreenOn из хранилища
+        cbScreenOn = settings.getString( "state_ScreenOn", null);    // достаем переменную из хранилища
+        if (cbScreenOn == null) {
+            cbScreenOn = "0";
+            editor.putString( "state_ScreenOn", cbScreenOn);   // положить переменную в хранилище
+            editor.commit();
+        }
+        if ((cbScreenOn.equals("1"))) {
+            checkBox_ScreenOn.setChecked(true);
+        } else {
+            checkBox_ScreenOn.setChecked(false);
+        }
+
+
+
+        //  Достаем checkBox_LaunchMaps из хранилища
+        cbLaunchMaps = settings.getString( "state_LaunchMaps", null);    // достаем переменную из хранилища
+        if (cbLaunchMaps == null) {
+            cbLaunchMaps = "0";
+            editor.putString( "state_LaunchMaps", cbLaunchMaps);   // положить переменную в хранилище
+            editor.commit();
+        }
+        if ((cbLaunchMaps.equals("1"))) {
+            checkBox_LaunchMaps.setChecked(true);
+        } else {
+            checkBox_LaunchMaps.setChecked(false);
+        }
+
+
 
         //  Достаем secretWord из хранилища
         secretWord = settings.getString( "secretWord", null);    // достаем переменную из хранилища
@@ -53,6 +112,8 @@ public class MainActivity extends AppCompatActivity {
             secretWord = "put secret word here";
         }
         tvSecretWord.setText(secretWord);
+
+
 
         //  Достаем слатьОтвет из хранилища
         sendResponse = settings.getString("sendResponse", null);
@@ -143,6 +204,28 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
         Toast.makeText(getApplicationContext(), "Secret word saved", Toast.LENGTH_SHORT).show();
     }
+
+
+    // Сохранить состояние чек бокса   checkBox_WakeUp
+    public void checkBox_WakeUp_OnClick(View v) throws InterruptedException {
+        editor.putString( "state_WakeUp", (this.checkBox_WakeUp.isChecked()) ? "1" : "0" );
+        editor.commit();
+    }
+
+
+    // Сохранить состояние чек бокса   checkBox_ScreenOn
+    public void checkBox_ScreenOn_OnClick(View v) throws InterruptedException {
+        editor.putString( "state_ScreenOn", (this.checkBox_ScreenOn.isChecked()) ? "1" : "0" );
+        editor.commit();
+    }
+
+
+    // Сохранить состояние чек бокса   checkBox_LaunchMaps
+    public void checkBox_LaunchMaps_OnClick(View v) throws InterruptedException {
+        editor.putString( "state_LaunchMaps", (this.checkBox_LaunchMaps.isChecked()) ? "1" : "0" );
+        editor.commit();
+    }
+
 
 
     //  Не используется. Для ручного старта broadcast receiver
